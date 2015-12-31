2b723b43c4fa9eb79546d8d2f73e36b6e7fee4f7
Donran <norrstrompontus@gmail.com>
2015-12-31 09:36:03 +0100

    Fixed various things and added showpoll

--- polls.js
+++ polls.js
@@ -6,7 +6,6 @@ function Poll() {
 		"endTime":0,
 		"timeLeft":0,
 		"options":[],
-		"optionsNum":[],
 		"text":[],
 		"creator":"",
 		"res":[],
@@ -21,10 +20,16 @@ Poll.prototype.pollCommand = function(msg, isMod) {
 		this.poll["timer"] = new Date().getTime();
 		//console.log("poll timer: "+this.poll["timer"]);
 		this.poll["options"] = msg.content.substring(5).split(",");
-		this.poll["optionsNum"] = this.poll["options"].length;
+		//clear spaces from !poll foo, bar
+		for(var i = 0; i < this.poll['options'].length; i++) {
+			if(this.poll['options'][i][0] == ' ') {
+				this.poll['options'][i] = this.poll['options'][i].substring(1);
+			}
+		}
+
 		this.poll["text"] = "A poll has been started by " + msg.author + " please enter a number. Type '!poll' to close.";
 
-		for(var i = 1; i < this.poll["optionsNum"] + 1; i++) {
+		for(var i = 1; i < this.poll['options'].length + 1; i++) {
 			this.poll["text"] += "\n" + i + ". ";
 			this.poll["text"] += this.poll["options"][i-1];
 		}
@@ -35,7 +40,7 @@ Poll.prototype.pollCommand = function(msg, isMod) {
 		this.poll["channel"] = msg.channel;
 		this.poll["creator"] = msg.author;
 		
-		for(var i = 0; i < this.poll["optionsNum"]; i++) {
+		for(var i = 0; i < this.poll['options'].length; i++) {
 			this.poll["res"][i] = 0;
 		}
 	} else if(this.poll["active"]) {
@@ -50,7 +55,7 @@ Poll.prototype.pollCommand = function(msg, isMod) {
 			returnText =  "this poll needs to be closed by " + this.poll["creator"].username + " or by anyone in " + this.poll["timeLeft"] + " seconds.";
 		}
 	} else {
-		returnText = "No poll is active, start a poll by typing !poll <option1>, <option2>, <option3>...";
+		returnText = "No poll is active, start a poll by typing !poll option1, option2, option3...";
 	}
 	return returnText;
 }
@@ -59,7 +64,7 @@ Poll.prototype.closePoll = function() {
 	if(this.poll["votes"] === 0) {
 		this.poll["text"] += "There were no votes for this poll\n";
 	} else {
-		for(i = 1 ; i < this.poll["optionsNum"] + 1; i++) {
+		for(i = 1 ; i < this.poll['options'].length + 1; i++) {
 
 			var percentage = this.poll["res"][i-1]/this.poll["votes"]*100;
 			this.poll["text"] += Math.round(percentage);
@@ -88,7 +93,6 @@ Poll.prototype.closePoll = function() {
 		"endTime":0,
 		"timeLeft":0,
 		"options":[],
-		"optionsNum":[],
 		"text":[],
 		"creator":"",
 		"res":[],
@@ -99,13 +103,44 @@ Poll.prototype.closePoll = function() {
 }
 Poll.prototype.vote = function(msg) {
 	if(msg.channel === this.poll["channel"]) {
-		if((parseInt(msg.content) < (this.poll["optionsNum"] + 1)) && (this.poll["users"].indexOf(msg.author) < 0) && ((parseInt(msg.content) != 0))) {
+		if((parseInt(msg.content) < (this.poll['options'].length + 1)) && (this.poll["users"].indexOf(msg.author) < 0) && ((parseInt(msg.content) != 0))) {
 			this.poll["res"][parseInt(msg.content) - 1]++;
 			this.poll["users"].push(msg.author);
 			this.poll["votes"]++;	
 		}
 	}
 } 
+
+Poll.prototype.showPoll = function() {
+	if(!this.poll['active'])
+		return "No poll is active, start a poll by typing !poll option1, option2, option3...";
+	var showPoll = "\n-------------" + "\n" + "Active poll" + "\n" + "-------------" + "\n";
+	if(this.poll['votes'] > 0) {
+		for(i = 1 ; i < this.poll["options"].length + 1; i++) {
+			var percentage = this.poll["res"][i-1]/this.poll["votes"]*100;
+			showPoll += Math.round(percentage);
+
+			if(percentage < 10) {
+				showPoll += "%\t\t\t"
+			} else if(percentage === 100) {
+				showPoll += "%\t\t"
+			} else if((percentage < 100) && (percentage >= 10)) {
+				showPoll += "%\t\t\t"
+			}
+			if(this.poll["res"][i-1] === 1) {
+				showPoll += this.poll["options"][i-1] + " (" + this.poll["res"][i-1] + " vote)" +"\n";
+			} else {
+				showPoll += this.poll["options"][i-1] + " (" + this.poll["res"][i-1] + " votes)" +"\n";
+			}
+		}
+		return showPoll;
+	} else {
+		for(var i = 1; i < this.poll['options'].length + 1;i++) {
+			showPoll += i+'. '+this.poll['options'][i-1] + " (0 votes)\n";
+		}
+		return showPoll;
+	}
+}
 
 // export class
 module.exports = Poll;
