5732e3e918321026a1e7707ca0e07afb44fa7611
swiftlyanerd <fearlessswiftie1322@gmail.com>
2017-10-18 19:25:42 -0700

    skeleton

--- rwMusicBot.js
+++ rwMusicBot.js
@@ -1,1 +1,25 @@
+const Discord = require('discord.js');
+const config = require('./auth.json');
 
+const bot = new Discord.Client();
+
+bot.on('ready', () => {
+	console.log('LHWB is ready!');
+});
+
+bot.on('message', message => {
+	var command = message.content.split(" ");
+	var params = command.slice(1, command.length).join(" ");
+
+	switch(command[0].toLowerCase()) {
+		case "!lversion":
+			versionCommand(message);
+			break;
+	}
+});
+
+function versionCommand(message) {
+	message.channel.send("Running version: " + config.bot.version);
+}
+
+bot.login(config.bot.token);
