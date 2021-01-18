90363fb677981fdf91ad102c915e60a2b73e8330
andrewc2 <andrewc2@users.noreply.github.com>
2021-01-18 17:34:34 -0500

    Delete actual-rewrite directory

--- actual-rewrite/commands/template.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-
-module.exports = {
-	name: "template",
-	description: "Template command.",
-	execute(lhwb, message, params) {
-		message.channel.send("Template command.");
-	},
-};
