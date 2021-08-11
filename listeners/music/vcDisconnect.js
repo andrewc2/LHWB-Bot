75d9a5b2582d7316c5aaa3ec4484dcbc21db6b71
andrewc2 <andy@iandrewc.com>
2021-08-11 09:48:53 -0400

    works now

--- listeners/music/vcDisconnect.js
+++ listeners/music/vcDisconnect.js
@@ -12,36 +12,23 @@ class VCDisconnectListener extends Listener {
 
     exec(oldState, newState) {
 
-        const connection = getVoiceConnection(this.client.guild.id);
-        connection.on(VoiceConnectionStatus.Disconnected, async (oldState, newState) => {
-            try {
-                await Promise.race([
-                    entersState(connection, VoiceConnectionStatus.Signalling, 5_000),
-                    entersState(connection, VoiceConnectionStatus.Connecting, 5_000),
-                ]);
-                // Seems to be reconnecting to a new channel - ignore disconnect
-            } catch (error) {
-                // Seems to be a real disconnect which SHOULDN'T be recovered from
-                connection.destroy();
-            }
-        });
+        if (newState.id === this.client.user.id && newState.connection === null) {
+            const connection = getVoiceConnection(oldState.guild.id);
 
-        /* if (newState.id === this.client.user.id && newState.connection === null) {
-            const channel = this.client.channels.cache.get(config.discord.channelID)
-            if (!channel) return console.log("I cannot find the voice channel.")
-            function disconnect(client) {
-                channel.join()
-                    .then(connection => {
-                        console.log("Reconnected to voice channel. - Disconnect");
-                        const broadcast = client.voice.broadcasts[0]
-                        connection.play(broadcast)
-                    })
-                    .catch(err => {
-                        console.log(`Error On Join: ${err}`)
-                    })
-            }
-            setTimeout(disconnect, 5000, this.client)
-        } */
+            connection.on(VoiceConnectionStatus.Disconnected, async (oldState, newState) => {
+                try {
+                    await Promise.race([
+                        entersState(connection, VoiceConnectionStatus.Signalling, 5_000),
+                        entersState(connection, VoiceConnectionStatus.Connecting, 5_000),
+                    ]);
+                    // Seems to be reconnecting to a new channel - ignore disconnect
+                } catch (error) {
+                    // Seems to be a real disconnect which SHOULDN'T be recovered from
+                    console.log(error);
+                    connection.destroy();
+                }
+            });
+        }
     }
 }
 
