cfbd9d8f74d6d1d8642063140be767fff199266c
andrewc2 <andy@iandrewc.com>
2021-10-16 14:58:13 -0400

    Handle member joins, rather than allow discord's annoying wave stickers

---
+++ listeners/client/user-join-listener.js
@@ -0,0 +1,26 @@
+const { Listener } = require("discord-akairo");
+const { MessageEmbed } = require("discord.js");
+
+class memberJoinListener extends Listener {
+	constructor() {
+		super("memberJoin", {
+			event: "guildMemberAdd",
+			emitter: "client",
+		});
+	}
+
+	exec(member) {
+        const embed = new MessageEmbed()
+            .setColor("#FF69B4")
+
+		if (member.guild.id === "689935210254696564") {
+			if (member.guild.systemChannelId === null){
+				console.log("Member joined but there is no system channel");
+				return;
+			}			
+            member.guild.systemChannel.send({ embeds: [embed.setDescription(`Welcome to the server, <@!${member.user.id}>! :wave:`)] });
+	    }
+    }
+}
+
+module.exports = memberJoinListener;
