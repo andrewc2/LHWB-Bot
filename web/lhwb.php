f6f48a3d2f4eb378fd93fa2949cffb0cfb912f16
andrewc2 <andy@iandrewc.com>
2021-10-18 21:19:20 -0400

    Basic header template, added privacy policy
    All pages now import a header rather than duplicate the code.
    New Privacy Policy
    Updated header color

--- web/lhwb.php
+++ web/lhwb.php
@@ -1,110 +1,47 @@
 <?php
-require_once "config.php";
-
-$db = new mysqli(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME);
-$db->set_charset("utf8");
+    $title = "LHWB Available Track Listing";
+    $desc = "Songs that are queueable on LosingHimWasBlue [Bot]";
+    require_once('includes/header.php');
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <!-- Primary Meta Tags -->
-    <title>LHWB Available Track Listing</title>
-    <meta name="title" content="LHWB Available Track Listing">
-    <meta name="description" content="Songs that are queueable on LosingHimWasBlue [Bot]">
-
-    <!-- Open Graph / Facebook -->
-    <meta property="og:type" content="website">
-    <meta property="og:url" content="https://lhwb.dev/lhwb.php">
-    <meta property="og:title" content="LHWB Available Track Listing">
-    <meta property="og:description" content="Songs that are queueable on LosingHimWasBlue [Bot]">
-    <meta property="og:image" content="https://i.imgur.com/NxrK0v4.jpeg">
-
-    <!-- Twitter -->
-    <meta property="twitter:card" content="summary_large_image">
-    <meta property="twitter:url" content="https://lhwb.dev/lhwb.php">
-    <meta property="twitter:title" content="LHWB Available Track Listing">
-    <meta property="twitter:description" content="Songs that are queueable on LosingHimWasBlue [Bot]">
-    <meta property="twitter:image" content="https://i.imgur.com/NxrK0v4.jpeg">
-
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs-3.3.7/jq-2.2.4/dt-1.10.13/datatables.min.css"/>
-    <link rel="stylesheet" type="text/css" href="darkly.css"/>
-
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-EJ2HKNBZ1B"></script>
-    <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-EJ2HKNBZ1B');
-    </script>
-    
-</head>
-<body>
-<div class="container">
-    <nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">LHWB</a>
+            <div id="dataDisplay">
+                <h1>Song List</h1>
+                <table class="table table-striped" id="songList">
+                    <thead>
+                    <tr>
+                        <th>Song Name</th>
+                        <th>Artist</th>
+                        <th>Album</th>
+                        <th>Play Count</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $stmt = $db->prepare("SELECT official_name, artist_name, album, play_count FROM song_detail WHERE 1 ORDER BY official_name");
+                        $stmt->execute();
+                        $stmt->bind_result($name, $artist, $album, $playcount);
+                        while ($stmt->fetch()) {
+                            echo "<tr><td>".htmlspecialchars($name)."</td><td>".htmlspecialchars($artist)."</td><td>".htmlspecialchars($album)."</td><td>".htmlspecialchars($playcount)."</td></tr>";
+                        }
+                        ?>
+                    <tfoot>
+                    <tr>
+                        <th>Song Name</th>
+                        <th>Artist</th>
+                        <th>Album</th>
+                        <th>Play Count</th>
+                    </tr>
+                    </tfoot>
+                </table>
             </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li><a href="index.php">Home</a></li>
-                    <li><a href="lhwb.php">Song List</a></li>
-                    <li><a href="recent.php">Recently Played Songs</a></li>
-                    <li><a href="calendar.php">Event Calendar</a></li>
-                </ul>
-            </div><!-- /.navbar-collapse -->
-        </div><!-- /.container-fluid -->
-    </nav>
-<div id="dataDisplay">
-    <h1>Song List</h1>
-    <table class="table table-striped" id="songList">
-        <thead>
-        <tr>
-            <th>Song Name</th>
-            <th>Album</th>
-            <th>Play Count</th>
-        </tr>
-        </thead>
-        <tbody>
-            <?php
-            $stmt = $db->prepare("SELECT official_name, artist_name, album, play_count FROM song_detail WHERE 1 ORDER BY official_name");
-            $stmt->execute();
-            $stmt->bind_result($name, $artist, $album, $playcount);
-            while ($stmt->fetch()) {
-                echo "<tr><td>".htmlspecialchars($name)."</td><td>".htmlspecialchars($artist)."</td><td>".htmlspecialchars($album)."</td><td>".htmlspecialchars($playcount)."</td></tr>";
-            }
-            ?>
-        <tfoot>
-        <tr>
-            <th>Song Name</th>
-            <th>Artist</th>
-            <th>Album</th>
-            <th>Play Count</th>
-        </tr>
-        </tfoot>
-    </table>
-</div>
-</div>
-<script type="text/javascript" src="https://cdn.datatables.net/v/bs-3.3.7/jq-2.2.4/dt-1.10.13/datatables.min.js"></script>
-<script>
-    $(document).ready(function() {
-       $('#songList').DataTable({
-           "iDisplayLength": 25,
-           "lengthMenu": [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]]
-       });
-    });
-</script>
-</body>
+        </div>
+        <script type="text/javascript" src="https://cdn.datatables.net/v/bs-3.3.7/jq-2.2.4/dt-1.10.13/datatables.min.js"></script>
+        <script>
+            $(document).ready(function() {
+            $('#songList').DataTable({
+                "iDisplayLength": 25,
+                "lengthMenu": [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]]
+            });
+            });
+        </script>
+    </body>
 </html>
---
+++ web/privacy.php
@@ -0,0 +1,105 @@
+<?php
+    $title = "LosingHimWasBlue [Bot] Privacy Policy";
+    $desc = "Privacy Policy for LosingHimWasBlue.";
+    require_once('includes/header.php');
+?>
+
+            <div id="dataDisplay">
+                <h3>LosingHimWasBlue [Bot] Privacy Policy</h1>
+                <p>Last updated: 18 October, 2021</p></br>
+
+                <p style="text-align:justify">
+                    This Privacy Policy sets out the policy which governs use of information you provide in connection with LosingHimWasBlue. The terms "you" and "your"
+                    refer to all individuals or entities using LosingHimWasBlue. The terms "we," "us," "our," refer to LosingHimWasBlue and "bot" refers to our Discord bot itself.
+                    If you have additional questions or require more information about our Privacy Policy, do not hesitate to ask by joining the bots support server.
+                </p>
+
+                <h5>Consent</h5>
+
+                <p style="text-align:justify">
+                    By using LosingHimWasBlue, you hereby consent to our Privacy Policy and agree to its terms.
+                </p>
+
+                <h5>Information we collect</h5>
+
+                <p style="text-align:justify">
+                    The personal information that you are asked to provide, and the reasons why you are asked to provide it,
+                    will be made clear to you at the point we ask you to provide your personal information. If you contact us directly,
+                    we may receive additional information about you such as your name, the contents of the message and/or attachments you
+                    may send us, and any other information you may choose to provide. When you use the bot, information that will be
+                    automatically collected are Discord user ID's, Discord usernames, Discord channel ID's, and Discord guild ID's.
+                    Other information may be collected through certain commands but this is optional.
+                </p>
+
+                <h5>How we use your information</h5>
+                <p>
+                    We use the information we collect in various ways, including to:
+                </p>
+
+                <ul> 
+                    <li>Provide, operate, and maintain our bot</li>
+                    <li>Improve, personalise, and expand our bot</li>
+                    <li>Understand and analyse how you use our bot</li>
+                    <li>Develop new features, and functionality</li>
+                </ul>
+
+                <p style="text-align:justify">
+                    LosingHimWasBlue follows a procedure of logging commands used. These commands are logged when executed.
+                    Information collected on each log are Discord user ID's, Discord username's, command name's, arguments provided's (if applicable),
+                    and the date's the command was used.
+                </p>
+
+                <h5>Third Party Privacy Policies</h5>
+
+                <p style="text-align:justify">
+                    LosingHimWasBlue's Privacy Policy does not apply to Discord as a whole or any third party API's that the bot may use. Third party API's include: Last.fm, Genius, top.gg, and Reddit. 
+                    Please note that LosingHimWasBlue has no access to or control over how information that Discord collects is used by third-party advertisers.
+                    Thus, we are advising you to consult the respective <a href="https://discord.com/privacy">Privacy Policy</a> and <a href="https://discord.com/terms">Terms of Service</a> of
+                    Discord for more detailed information. It may include their practices and instructions about how to opt-out of certain options.
+                </p>
+                
+                <h5>CCPA Privacy Rights (Do Not Sell My Personal Information)</h5>
+
+                <p>
+                    Under the CCPA, among other rights, California consumers have the right to:
+                </p>
+
+                <ul>
+                    <li>Request that a business that collects a consumer's personal data disclose the categories and specific pieces of personal data that a business has collected about consumers.</li>
+                    <li>Request that a business delete any personal data about the consumer that a business has collected.</li>
+                    <li>Request that a business that sells a consumer's personal data, not sell the consumer's personal data.</li>
+                </ul>
+
+                <p>
+                    If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.
+                </p>
+
+                <h5>GDPR Data Protection Rights</h5>
+
+                <ul>
+                    <li>The right to access – You have the right to request copies of your personal data.</li>
+                    <li>The right to rectification – You have the right to request that we correct any information you believe is inaccurate.
+                        You also have the right to request that we complete the information you believe is incomplete.
+                    </li>
+                    <li>The right to erasure – You have the right to request that we erase your personal data, under certain conditions.</li>
+                    <li>The right to restrict processing – You have the right to request that we restrict the processing of your personal data, under certain conditions.</li>
+                    <li>The right to object to processing – You have the right to object to our processing of your personal data, under certain conditions.</li>
+                    <li>The right to data portability – You have the right to request that we transfer the data that we have collected to another organisation,
+                        or directly to you, under certain conditions. If you make a request, we have one month to respond to you. If you would like
+                        to exercise any of these rights, please contact us.
+                    </li>
+                </ul>
+
+                <h5>Children's Information</h5>
+
+                <p>
+                    Another part of our priority is adding protection for children while using the internet.
+                    We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity. LosingHimWasBlue does not 
+                    knowingly collect any Identifiable Information from children under the age of 13. Please see the <a href="https://discord.com/terms">Discord Terms of Service</a> for more information.
+                    If you think that your child provided this kind of information through the bot, we strongly encourage you to contact us immediately and we will
+                    do our best efforts to promptly remove such information from our records.
+                </p>
+            </div>
+        </div>
+    </body>
+</html>
