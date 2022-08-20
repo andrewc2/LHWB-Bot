599cc44e2a9456ebf22d0d4fac87048ebe20ce9c
andrewc2 <andy@iandrewc.com>
2022-08-20 16:48:27 -0400

    gif slash

---
+++ slashCommands/other/gifSlashCommand.js
@@ -0,0 +1,29 @@
+const { SlashCommand } = require('discord-akairo');
+const { EmbedBuilder } = require('discord.js');
+const { db } = require('../../models/db');
+
+module.exports = class gifSlashCommand extends SlashCommand {
+  constructor() {
+    super('gif', {
+      name: 'gif',
+      prefixId: 'gif',
+      category: 'other',
+      commandType: 'command',
+      description: 'Displays a random Taylor related gif.',
+    });
+  }
+
+  async exec(interaction) {
+    db.query('SELECT path, type FROM media WHERE type = \'gif\' ORDER BY RAND() LIMIT 1', function(err, rows) {
+        const embed = new EmbedBuilder()
+          .setColor('#FF69B4')
+          .setImage(`${rows[0].path}`)
+          .setFooter({
+            text: `Submit gifs to be added using: /request gif [imgur url]`,
+          });
+          return interaction.reply({ embeds: [embed] });
+      });
+
+    
+  }
+};
