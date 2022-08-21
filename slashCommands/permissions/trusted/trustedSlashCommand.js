f5d270d37dad0a13f09db8a75781bf97258555f6
username13 <username130711@gmail.com>
2022-08-21 14:43:21 +0100

    ported trusted commands to slash commands

---
+++ slashCommands/permissions/trusted/trustedSlashCommand.js
@@ -0,0 +1,30 @@
+const { SlashCommand } = require('discord-akairo');
+const { ApplicationCommandOptionType } = require('discord.js');
+
+module.exports = class TrustedSlashCommand extends SlashCommand {
+  constructor() {
+    super('trusted', {
+      name: 'trusted',
+      commandType: 'command',
+      category: 'permissions',
+      description: 'Set, clear, and view the trusted role',
+      slashOptions: [
+        {
+          name: 'set',
+          description: 'Set the trusted role',
+          type: ApplicationCommandOptionType.Subcommand,
+        },
+        {
+          name: 'clear',
+          description: 'Clear the trusted role',
+          type: ApplicationCommandOptionType.Subcommand,
+        },
+        {
+          name: 'view',
+          description: 'View the trusted role',
+          type: ApplicationCommandOptionType.Subcommand,
+        },
+      ],
+    });
+  }
+};
