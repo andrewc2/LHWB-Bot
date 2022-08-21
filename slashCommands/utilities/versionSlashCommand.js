a2eb2cd4003f531c4fc9a074d43f297ecec8a7ed
username13 <username130711@gmail.com>
2022-08-21 16:38:18 +0100

    port version to slash command

---
+++ slashCommands/utilities/versionSlashCommand.js
@@ -0,0 +1,32 @@
+const { SlashCommand } = require('discord-akairo');
+const { EmbedBuilder } = require('discord.js');
+const { db } = require('../../models/db');
+
+module.exports = class VersionSlashCommand extends SlashCommand {
+  constructor() {
+    super('version', {
+      name: 'version',
+      commandType: 'command',
+      prefixId: 'lversion',
+      description: 'Displays version information for the bot',
+      category: 'other',
+    });
+  }
+
+  async exec(interaction) {
+    await interaction.deferReply();
+    db.query('SELECT * FROM version', function(err, rows) {
+      const embed = new EmbedBuilder()
+      // pink
+        .setColor('#FF69B4')
+        .setTitle('Patch Notes:')
+        .setAuthor({
+          name: `Version: ${rows[0]['versionNum']}`,
+          iconURL: 'https://lhwb.dev/ts.png',
+          url: 'https://github.com/andrewc2/LHWB-Bot/wiki/Change-Log',
+        })
+        .setDescription(`${rows[0]['patchNotes']}`);
+      return interaction.editReply({ embeds: [embed] });
+    });
+  }
+};
